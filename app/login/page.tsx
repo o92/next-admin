bbec75943c394b6911a625071d9759f4c0f68d90
o92 <huyuanjing1998@icloud.com>
2024-08-19 15:52:39 +0800

    add puzzle-captcha Verify and mode-toggle component to /login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,24 +9,24 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { ModeToggle } from "@/components/mode-toggle";
-import { cookies } from "next/headers";
+import Verify from "@/components/verify/puzzle-captcha";
 
 export default async function Page() {
 	'use server'
 	return (
 		<div className="grid h-screen w-full place-content-center">
 			<form action={createInvoice}>
-				<Card className=" w-96 relative">
-					<ModeToggle className="absolute top-0 right-0"></ModeToggle>
+				<Card className="w-96 relative">
 					<CardHeader>
-						<CardTitle className="text-2xl">登录</CardTitle>
+						<CardTitle className="relative text-2xl">登录<ModeToggle className="absolute right-0"></ModeToggle></CardTitle>
+						
 						<CardDescription>
 							请输入您的账号和密码进行登录.
 						</CardDescription>
 					</CardHeader>
 					<CardContent className="grid gap-4">
 						<div className="grid gap-2">
-							账号<Input name="email" type="text" required />
+							账号<Input name="username" type="text" required />
 						</div>
 						<div className="grid gap-2">
 							密码<Input name="password" type="password" required />
@@ -36,6 +36,7 @@ export default async function Page() {
 						<Button className="w-full">登陆</Button>
 					</CardFooter>
 				</Card>
+				<Verify></Verify>
 			</form>
 		</div>
 	)
@@ -43,6 +44,7 @@ export default async function Page() {
 
 async function createInvoice(formData:FormData) {
 	'use server'
-	cookies().delete("a");
+	// cookies().delete("a");
+
 	return formData;
 }
